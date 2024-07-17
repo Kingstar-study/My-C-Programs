b75f3dcd2fd6a96fed6de666b506f286343ba351
Kingstar-study <kingstarstudy@gmail.com>
2024-07-17 20:08:26 +0600

    modified3.1

--- Chapter 3 practice set/03_problem1.c
+++ Chapter 3 practice set/03_problem1.c
@@ -1,20 +1,20 @@
 /* Author  : Rizwan
-   Problem :What will be the output of this program 
-int a = 10; 
-if (a = 11) 
-printf("I am 11"); 
-else  
+   Problem :What will be the output of this program
+int a = 10;
+if (a = 11)
+printf("I am 11");
+else
 printf("I am not 11"); */
 
 #include <stdio.h>
 
 int main()
 {
-    // What will be the output of this program 
-int a = 10; 
-if (a = 11) 
-printf("I am 11"); 
-else  
-printf("I am not 11");
+    // What will be the output of this program
+    int a = 10;
+    if (a = 11)
+        printf("I am 11");
+    else
+        printf("I am not 11");
     return 0;
 }
---
+++ Chapter 3 practice set/03_problem2.c
@@ -0,0 +1,12 @@
+/* Author  : Rizwan
+   Problem : Write a program to determine whether a student has passed or failed. To pass, a 
+student requires a total of 40% and at least 33% in each subject. Assume there 
+are three subjects and take the marks as input from the user.*/
+
+#include <stdio.h>
+
+int main()
+{
+    
+    return 0;
+}
